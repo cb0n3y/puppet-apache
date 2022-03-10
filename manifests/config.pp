89c1e1f5e110dd2e22b6603cac720f96bb6ed6eb
cb0n3y <cb0n3y@gmail.com>
2022-03-10 07:53:00 +0100

    add a loop and change config_path to config_paths

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,7 +5,9 @@
 # @example
 #   include apache::config
 class apache::config {
-  file { $apache::config_path: 
-    ensure  => directory,
+  $apache::config_paths.each |Array[String] $config_path| {
+    file { $apache::config_path: 
+      ensure  => directory,
+    }
   }
 }
