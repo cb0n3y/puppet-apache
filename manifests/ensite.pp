aa2fe8a3eeb838705246f830a4525cf0f6d0f1dd
cb0n3y <cb0n3y@gmail.com>
2022-03-11 07:06:46 +0100

    fix errors in ensite.pp manifest

--- manifests/ensite.pp
+++ manifests/ensite.pp
@@ -4,5 +4,13 @@
 #
 # @example
 #   include apache::ensite
-class apache::ensite {
+class apache::ensite (
+  String $vhost_file = '000-default.conf'
+) {
+  if $facts['os']['family'] == 'Debian' {
+    exec { "a2ensite ${vhost_file}":
+      onlyif => "test -f /etc/apache2/sites-available/${vhost_file}",
+      path   => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+    }
+  }
 }
