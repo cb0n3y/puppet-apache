1b594e0c8b64579c4938bda4cae6c044ca2a464d
cb0n3y <cb0n3y@gmail.com>
2022-03-11 06:47:23 +0100

    add required changes in init.,pp manifest to run service.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,10 +7,15 @@
 class apache (
   String   $install_name,
   Array    $config_paths,
+  String   $service_name,
 ) {
   contain apache::install
   contain apache::config
+  contain apache::service
  
-  Class['apache::install'] 
+  Class['apache::install']
+  # class apache::install runs before apache::config
    -> Class['apache::config']
+  # ~> == subscribe, restart the service everytime the config has been changed
+   ~> Class['apache::service']
 }
