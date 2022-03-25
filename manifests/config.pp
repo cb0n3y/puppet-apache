673b879f4c48d2516ea1ea20de8f061ee88eec4c
cb0n3y <cb0n3y@gmail.com>
2022-03-25 06:22:10 +0100

    Add the rest

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,7 +6,7 @@
 #   include apache::config
 class apache::config {
   $apache::config_paths.each |String $config_path| {
-    file { $config_path: 
+    file { $config_path:
       ensure  => directory,
     }
   }
--- spec/classes/apache_spec.rb
+++ spec/classes/apache_spec.rb
@@ -7,6 +7,22 @@ describe 'apache' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
+      it { is_expected.to contain_class('apache::install') }
+      it { is_expected.to contain_class('apache::config') }
+      it { is_expected.to contain_class('apache::service') }
+
+      case os_facts[:osfamily]
+      when 'Debian'
+        it { is_expected.to contain_package('apache2').with_ensure('present') }
+	it { is_expected.to contain_service('apache2').with_ensure('running') }
+	it { is_expected.to contain_file('/etc/apache2/sites-enabled').with_ensure('directory') }
+	it { is_expected.to contain_file('/etc/apache2/sites-available').with_ensure('directory') }
+      when 'RedHat'
+	it { is_expected.to contain_package('httpd').with_ensure('present') }
+	it { is_expected.to contain_service('httpd').with_ensure('running') }
+	it { is_expected.to contain_file('/etc/httpd/conf.d').with_ensure('directory') }
+      end
+
       it { is_expected.to compile }
     end
   end
